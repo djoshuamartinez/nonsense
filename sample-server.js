93f12d95b2aa6d30c903f33852791bb188253e35
Joshua Martínez <djoshuamartinezpineda@gmail.com>
2018-11-29 14:04:51 -0600

    Sample of a server with pure nodejs

---
+++ sample-server.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function(request, response){
+    console.log('request ', request.url);
+    var filePath = '.'+request.url;
+    if(filePath == './'){
+	filePath = './index.html';
+    }
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+	'.html': 'text/html',
+	'.js': 'text/javascript'
+    };
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+    fs.readFile(filePath, function(error, content){
+	if(error){
+	    if(error.code == 'ENOENT'){
+		fs.readFile('./404.html', function(error, content){
+		    response.writeHead(200, {'Content-Type':contentType});
+		    response.end(content, 'utf-8');
+		});
+	    }
+	    else {
+		response.writeHead(500);
+		response.end('Internal server error '+error.code+'\n');
+		response.end();
+	    }
+	}
+	else {
+	    response.writeHead(200, {'Content-Type': contentType});
+	    response.end(content, 'utf-8');
+	}
+    });
+    
+}).listen(8125);
